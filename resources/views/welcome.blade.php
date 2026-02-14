5544ef47746a902cb16bb422b32471c6917d7db2
Syalikhafaradhiya Agira <syalikhafa@gmail.com>
2026-02-14 07:01:58 +0700

    Modif 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,8 +54,9 @@
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <h1 class="mb-1 font-medium">SYALIKHAFARADHIYA AGIRA</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">NIM 251402112</p>
-                    <h1 class="mb-1 font-medium">TEKNOLOGI INFORMASI S-1 </h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">FAKULTAS ILMU KOMPUTER & TEKNOLOGI INFORMASI </p>
+                    <h1 class="mb-1 font-medium">Universitas Sumatera Utara</h1>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Major - Information Technology </p>
+                    <h1 class="mb-1 font-medium">Exploring Technology & Programming</h1>
                 </div>
                 <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ml-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     {{-- Laravel Logo --}}
